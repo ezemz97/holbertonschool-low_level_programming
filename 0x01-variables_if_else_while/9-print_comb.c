233a8dd0d7f12c6a6c47dc75fbca055c23817659
ezedksl <ezedksl@gmail.com>
2020-09-17 21:21:22 +0000

    added 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar (n);
+if (n != 57)
+{
+putchar (44);
+putchar (32);
+}
+}
+putchar ('\n');
+return (0);
+}

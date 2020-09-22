bdac15cd678966cd2e85402b2fbbb142f5f13bac
ezedksl <ezedksl@gmail.com>
2020-09-22 14:37:26 +0000

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
-
+/* print_sign */
 int print_sign(int n)
 {
-if ( n > 0 )
+if (n > 0)
 {
 _putchar('+');
 return (1);
 }
-else if ( n == 0 )
+else if (n == 0)
 {
 _putchar('0');
- return (0);
+return (0);
 }
 else
 {
 _putchar('-');
- return (-1);
+return (-1);
 }
 }

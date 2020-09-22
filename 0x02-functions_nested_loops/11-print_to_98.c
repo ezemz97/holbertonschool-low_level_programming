e33e6fd974ff3ff10ac56cfaecca271bf77e1853
ezedksl <ezedksl@gmail.com>
2020-09-22 19:03:54 +0000

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+B#include "holberton.h"
 #include <stdio.h>
 
 /**
@@ -9,23 +9,23 @@
 
 void print_to_98(int n)
 {
-  int x;
+int x;
 while (x != 98)
 {
-      if (n > 98)
-	{
-	  printf ("%d, ", n);
-	  n--;
-	    }
-      else if (n < 98)
-	{
-	  printf ("%d, ", n);
-	  n++;
-	    }
-  else 
+if (n > 98)
 {
-  printf ("%d\n", n);
-  x = n;
+printf("%d, ", n);
+n--;
+}
+else if (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+else
+{
+printf("%d\n", n);
+x = n;
 }
 }
 }

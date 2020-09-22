e0bf58821efc00ce6a5faaf0835502ae98371fe2
ezedksl <70861611+ezedksl@users.noreply.github.com>
2020-09-22 15:51:01 -0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,14 @@
 #include "holberton.h"
-
+/**
+ * times_table - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    times_table();
+    return (0);
+}
 void times_table(void)
 {
   int a, b, x;

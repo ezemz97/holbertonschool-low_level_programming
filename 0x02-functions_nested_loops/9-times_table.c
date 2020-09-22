26a0cbe83a734af05d87195f0114be94e161c332
ezedksl <ezedksl@gmail.com>
2020-09-22 18:49:18 +0000

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,35 +6,30 @@
  * Return: Always 0.
  */
 
-int main(void)
-{
-    times_table();
-    return (0);
-}
 void times_table(void)
 {
-  int a, b, x;
-  for (a = 0; a <= 9; a++)
-    {
-      _putchar('0');
-      for (b = 1; b <= 9; b++)
-	{
-	  x = a * b;
-	  if (x < 10)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	      _putchar(' ');
-	      _putchar('0' + x);
-	    }
-	  else if (x >= 10)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	      _putchar('0' + x / 10);
-	      _putchar('0' + x % 10);
-	    }
-	}
-      _putchar('\n');
-    }
+int a, b, x;
+for (a = 0; a <= 9; a++)
+{
+_putchar('0');
+for (b = 1; b <= 9; b++)
+{
+x = a * b;
+if (x < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + x);
+}
+else if (x >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar('0' + x / 10);
+_putchar('0' + x % 10);
+}
+}
+_putchar('\n')
+}
 }

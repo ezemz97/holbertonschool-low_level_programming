6d7a19cb3eefbb9a894694a65866162aa6555946
ezedksl <ezedksl@gmail.com>
2020-09-22 14:48:32 +0000

    added

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 #include <stdio.h>
-
+/**
+ * _abs - check the code for Holberton School students.
+ * @r : in
+ * Return: Always 0.
+ */
 int _abs(int r)
 {
 if (r < 0)
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
-
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @r: ok
+ * Return: Always 0.
+ */
 int print_last_digit(int r)
 {
 int x = (r % 10);
 if (x < 0)
 {
-  x = x * (-1);
+x = x * (-1);
 }
 _putchar ('0' + x);
 return (x);

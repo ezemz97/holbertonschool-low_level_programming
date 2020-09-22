9e89b6a8a0d121323fdffb652bc9674772e99ed6
ezedksl <70861611+ezedksl@users.noreply.github.com>
2020-09-22 11:43:48 -0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
 #include "holberton.h"
-#include <ctype.h>
 
+/**
+ * _islower - check the code for Holberton School students.
+ *
+ * @c: variable
+ *
+ * Return: Always 0.
+ */
 int _islower(int c)
 {
-if ( islower(c) != 0 )
+if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
+else
 return (0);
 }

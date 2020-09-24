6fc4ee1c3920f0227bab0c912ccd6109dea24059
ezedksl <ezedksl@gmail.com>
2020-09-24 18:30:48 +0000

    fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int x;
+	for (x = 0; x < 10; x++)
+	{
+		_putchar('0' + x);
+	}
+	_putchar('\n');
+}

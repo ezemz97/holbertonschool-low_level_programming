c2bcc2915380b1575aee2638a7dd2dd97a9bfb1b
ezedksl <ezedksl@gmail.com>
2020-09-24 18:41:23 +0000

    added 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+		}
+		else
+		{
+		_putchar('0' + x);
+		}
+	}
+	_putchar('\n');
+}

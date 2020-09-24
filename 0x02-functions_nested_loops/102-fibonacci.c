ff987115b5f7d7c8c34bad74f99c6754a7463d95
Eze <eze@pop-os.localdomain>
2020-09-23 23:55:38 -0300

    dx

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int before = 1, after = 1, product, x;
+
+	for (x = 0, x < 48, x++)
+	{
+		printf("%d, ", before);
+		product = (after + before);
+		before = after
+	}
+}

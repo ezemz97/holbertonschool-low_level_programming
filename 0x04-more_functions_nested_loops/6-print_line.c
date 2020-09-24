6233c95a1fa2c2a4f290e1116798c2d46e8743f0
ezedksl <ezedksl@gmail.com>
2020-09-24 19:30:21 +0000

    added task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int x, z;
+	
+	for (x = 0; x < 15; x++)
+	{
+		for (z = 0; z < 15; z++)
+		{
+			if (z >= 10)
+			{
+				_putchar('0' + (z / 10));
+			}
+			_putchar('0' + (z % 10));
+		}
+		_putchar('\n');
+	}
+}

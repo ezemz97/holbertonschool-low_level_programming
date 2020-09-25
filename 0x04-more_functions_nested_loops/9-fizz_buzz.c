d7b821dd91ac201f22eb51483e3f99b61782daed
Eze <eze@pop-os.localdomain>
2020-09-24 23:44:04 -0300

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
-
 /**
- * main - entry point
+ * main - check the code for Holberton School students.
  *
- * Return: 0 (success)
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int a;
+int a;
 
-	for (a = 1; a <= 100; a++)
-	{
-		if ((a % 3 == 0) && (a % 5 == 0))
-			printf("FizzBuzz");
-		else if (a % 5 == 0)
-			printf("Buzz");
-		else if (a % 3 == 0)
-			printf("Fizz");
-		else
-			printf("%d", a);
-		if (a != 100)
-			printf(" ");
-	}
-	printf("\n");
+for (a = 1; a < 100; a++)
+if (a % 15 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", a);
+}
+printf("Buzz");
+printf("\n");
+return (0);
 }

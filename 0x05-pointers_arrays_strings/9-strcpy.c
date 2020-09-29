23e63b7b569cc8102d76ee041547126e605eba3d
ezedksl <ezedksl@gmail.com>
2020-09-29 20:05:29 +0000

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int x;
 
-	for (x = 0; src[x] >= '\0'; ++x)
+	for (x = 0; src[x] != '\0'; ++x)
 	{
 		dest[x] = src[x];
 	}
+	/* can do "dest[x] = src[x]" here to assign the null value because 'src[x]' is null after loop*/
+	dest[x++] = '\0';
 	return (dest);
 }

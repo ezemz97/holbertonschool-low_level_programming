d817e900634b043339a836fcf4f61cb33623eab6
ezedksl <ezedksl@gmail.com>
2020-10-01 17:17:32 +0000

    fix 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,20 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+ * _strcmp - Compare two strings (char x char) substracting their asscii values
+ * @s1: string 1
+ * @s2: string 2
+ * Return: Always 0.
+ */
+
 int _strcmp(char *s1, char *s2)
 {
+	int x = 0;
 
+	while (s1[x] != '\0' && s2[x] != '\0' && s1[x] == s2[x])
+	{
+		x++;
+	}
+	return (s1[x] - s2[x]);
+}

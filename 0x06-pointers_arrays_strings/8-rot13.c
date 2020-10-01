a8009f678355fd499c78d55e228e875e92fc5e3b
ezedksl <ezedksl@gmail.com>
2020-10-01 21:41:06 +0000

    added 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -2,13 +2,28 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * rot13 - check the code for Holberton School students.
+ * @str: string
  * Return: Always 0.
  */
 
-char *rot13(char *)
+char *rot13(char *str)
 {
-	int x;
+	int x, z;
+	char fh[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char sh[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		for (z = 0; fh[z] != '\0'; z++)
+		{
+			if (fh[z] == str[x])
+			{
+				str[x] = sh[z];
+				break;
+			}
+		}
+	}
+	return (str);
+}
 

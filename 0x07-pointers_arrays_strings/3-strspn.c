4b738ea3869cd7030e7b693aa898a852be84fdbe
ezedksl <ezedksl@gmail.com>
2020-10-05 18:54:29 +0000

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (x = 0; s[x] != 32; x++)
 	{
-		for (z = 0; accept[z] != '\0'; z++)
+		for (z = 0; accept[z] >= '\0'; z++)
 		{
 			if (s[x] == accept[z])
 			{

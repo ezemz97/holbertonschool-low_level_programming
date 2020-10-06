a256a2fea97b23aca5b3cc257d6b23b1d8b72c87
ezedksl <70861611+ezedksl@users.noreply.github.com>
2020-10-05 23:41:24 -0300

    fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * print_chessboard - print a chessboard 2d array.
  * @a: row pointer
  * Return: Always 0.
  */
+
 void print_chessboard(char (*a)[8])
 {
-int x, z;
-for (x = 0; x < 8; x++)
-{
-for (z = 0; z < 8; z++)
-{
-_putchar (a[x][z]);
-}
-_putchar ('\n');
-}
+	int x, z;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (z = 0; z < 8; z++)
+			_putchar(a[x][z]);
+		_putchar('\n');
+	}
 }

628687732a993499e54113ee46362a96f2552a38
ezedksl <70861611+ezedksl@users.noreply.github.com>
2020-10-08 09:29:19 -0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- * @s: 
- * Return: Always 0.
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s: var
+ * Return: void.
  */
 
 void _print_rev_recursion(char *s)

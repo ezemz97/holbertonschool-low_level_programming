a48c41e484bd01f9d3bf684ec83bdf77a3b65b8e
mi-kei-la <micaela.percovich@gmail.com>
2020-10-11 19:24:53 +0000

    add

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, __attribute__((unused))char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

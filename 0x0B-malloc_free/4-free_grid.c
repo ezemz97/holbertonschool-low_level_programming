7297ab8ebae67fc3f1b91d15a0714a58655186d9
ezedksl <ezedksl@gmail.com>
2020-10-14 23:00:21 +0000

    added 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * free_grid - Concatenate two strings
+  * @grid: grid
+  * @height: height
+  * Return: void
+  */
+
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0; x < height; x++)
+	{
+		free(grid[x]);
+	}
+	free(grid);
+}

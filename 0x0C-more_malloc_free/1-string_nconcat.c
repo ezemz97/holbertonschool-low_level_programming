aa77bcdc112205de085cefa5f9f8ba1660e91e4b
eze <ezedksl@gmail.com>
2020-10-15 19:34:50 +0000

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	while (s1[l1] != '\0')
 		l1++;
-	while (s1[l2] != '\0')
+	while (s2[l2] != '\0')
 		l2++;
 	p = malloc(l1 + l2 + 1);
 	if (p == NULL)

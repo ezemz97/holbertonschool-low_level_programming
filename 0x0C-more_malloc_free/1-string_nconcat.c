7e2ef51aebaee8abef104d3d828bbf10766d25d9
eze <ezedksl@gmail.com>
2020-10-15 19:42:30 +0000

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[x] = s1[x];
 		x++;
 	}
-	while (s2[z] != '\0' && z <= n)
+	while (s2[z] != '\0' && z < n)
 	{
 		p[x + z] = s2[z];
 		z++;

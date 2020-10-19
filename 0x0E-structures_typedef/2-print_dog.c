05eb0d63d99c7df8a98d8c993926abc7ed053a3e
ezedksl <ezedksl@gmail.com>
2020-10-19 19:10:05 +0000

    fix

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  * print_dog - function name
+  * @d: pointer
+  */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
+

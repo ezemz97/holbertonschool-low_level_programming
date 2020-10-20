6d8878751647b1830c6bd425356ef0113e385971
eze <ezedksl@gmail.com>
2020-10-20 04:19:08 +0000

    fix 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,22 +33,6 @@ char *_strdup(char *str)
 	return (strDUPE);
 	free(strDUPE);
 }
-
-/**
- * _strlen_recursion - check the code for Holberton School students.
- * @s: string
- * Return: Always 0.
- */
-
-int _strlen_recursion(char *s)
-{
-	if (*s)
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
-	return (0);
-}
-
 /**
   * new_dog - new struct dog
   * @name: dog name
@@ -59,30 +43,26 @@ int _strlen_recursion(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-
 	char *namecpy, *ownercpy;
-	int namelen = 0, ownerlen = 0;
-
-	namelen = _strlen_recursion(name);
-	ownerlen = _strlen_recursion(owner);
-
-	namecpy = malloc(sizeof(char) * (namelen + 1));
-	ownercpy = malloc(sizeof(char) * (ownerlen + 1));
-	if (namecpy == NULL || ownercpy == NULL)
-	{
-		free(namecpy);
-		free(ownercpy);
-		return (NULL);
-	}
-	namecpy = _strdup(name);
-	ownercpy = _strdup(owner);
-	if (namecpy == NULL || ownercpy == NULL)
-		return (NULL);
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
+	if (name == NULL || owner == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	namecpy = _strdup(name);
+	ownercpy = _strdup(owner);
+	if (namecpy == NULL || ownercpy == NULL)
+	{
+		free(namecpy);
+		free(ownercpy);
+		free(new_dog);
+		return (NULL);
+	}
 	new_dog->name = namecpy;
 	new_dog->age = age;
 	new_dog->owner = ownercpy;

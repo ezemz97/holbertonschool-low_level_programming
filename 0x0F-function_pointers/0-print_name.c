6ab1902a069f923c37e110f3ef941ba3197df588
ezedksl <ezedksl@gmail.com>
2020-10-21 17:20:06 +0000

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include "function_pointers.h"
+/**
+  * print_name - print name
+  * @f: function parameter
+  * @name: pointer to char str
+  */
 
 void print_name(char *name, void (*f)(char *))
 {

04824c1b3c51815921b0e7d577b55e31fa21eccb
eze <ezedksl@gmail.com>
2020-10-24 02:45:53 -0300

    tried

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,16 +6,44 @@
   */
 void print_all(const char * const format, ...)
 {
+	void (*fprints[4][2])() = {
+		{'c', fchar}
+		{'i', fint}}
+		{'f', ffloat}
+		{'s', fstring}
+	};
 	unsigned int x, z, count = 0;
-	va_list valist;
+	va_list args;
+
 
 	while (format[x] != '\0')
 	{
-		if (format[x] == 'c')
+		if (format[x] == 'c' || format[x] == 'i' || format[x] == 'f' || format[x] == 's')
 			count++;
 		x++;
 	}
 
-	va_start(valist, count);
-	while (formar[z] != 0 && <
+	va_start(args, count);
+	while (format[z] == fprints[z] && z <= 4)
+	{
+		printf(fprints[z][1]);
+		z++;
+	}
+}
 
+void fint(va_list args)
+{
+	printf("%i", va_arg(args, int));
+}
+void fchar(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+void ffloat(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+void fstring(va_list args)
+{
+	printf("%i", va_arg(args, char*));
+}

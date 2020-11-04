7dd994e56d396024f11a4b51f17ca708893da17b
eze <ezedksl@gmail.com>
2020-11-04 13:39:56 -0300

    added some conditions for 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,13 +12,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int x;
 	listint_t *cabeza, *new;
 
+	if (!head)
+		return (NULL);
 	cabeza = *head;
 	new = malloc(sizeof(listint_t));
+	if (!new)
+		free(new);
+		return (NULL);
 
 	for (x = 0; x <= idx; x++)
 	{
-		if (cabeza == NULL)
-			return (NULL);
 		if (x != idx - 1)
 			cabeza = cabeza->next;
 		else

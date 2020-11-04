6dd3c1f84da137d91e223a4e8e1f51d8e652c36c
eze <ezedksl@gmail.com>
2020-11-04 23:57:19 +0000

    fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,8 +25,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	for (x = 0; temp != NULL && x < (index - 1); x++)
-		if (temp == NULL)
-			return (-1);
 		temp = temp->next;
 
 	if (!temp || temp->next == NULL)
@@ -36,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	free(temp->next);
 	temp->next = next;
 
-	return (0);
+	return (1);
 }
 
 

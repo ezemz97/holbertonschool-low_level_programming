a77465db6eda6aa17212cc0dae886646de588f80
eze <ezedksl@gmail.com>
2020-11-04 23:58:26 +0000

    return 1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = temp->next;
 		free(temp);
-		return (0);
+		return (1);
 	}
 
 	for (x = 0; temp != NULL && x < (index - 1); x++)

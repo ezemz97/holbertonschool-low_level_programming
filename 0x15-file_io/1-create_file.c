f5d7290708515666d3c29d05681aba0c5a0683f4
ezedksl <ezedksl@gmail.com>
2020-11-09 19:22:15 +0000

    fix?

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,18 +9,17 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, text_size = 0, ret;
 
-	while (text_content[text_size] != '\0')
-		text_size++;
-
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
+		while (text_content[text_size] != '\0')
+			text_size++;
 		ret = write(fd, text_content, text_size);
 	}
 	close(fd);

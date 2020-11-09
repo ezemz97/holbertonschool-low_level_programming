479b7a7a3b7f5a5a7fa22b1ad2a4b3c12a71135b
ezedksl <ezedksl@gmail.com>
2020-11-09 19:19:30 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,12 +22,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		ret = write(fd, text_content, text_size);
-		if (ret == -1)
-		{
-			close(fd);
-			return (-1);
-		}
 	}
 	close(fd);
+	if (ret == -1)
+		return (-1);
+
 	return (1);
 }

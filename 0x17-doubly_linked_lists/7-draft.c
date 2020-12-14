6a158d5960ee7599dd6eadbffbed5698ae4bbdae
ezedksl <ezedksl@gmail.com>
2020-12-14 15:45:41 +0000

    update 7

---
+++ 0x17-doubly_linked_lists/7-draft.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - Insert node at given position
+ * @h: head of list
+ * @idx: index (position to add node to)
+ * @n: data to store in the new node
+ * Return: new node or null if error
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *head = *h;
+
+	if (!*h && idx != 0)
+		return (NULL);
+	while (idx--)
+	{
+		if (!head->next)
+			return (NULL);
+		head = head->next;
+	}
+	if (!head->next)
+	{
+		new = add_dnodeint_end(h, n);
+	}
+	else if (!head->prev && head->next != NULL)
+	{
+		new = add_dnodeint(h, n);
+	}
+	else
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (!new)
+			return (NULL);
+		new->n = n;
+		new->next = head;
+		new->prev = head->prev;
+		head->prev->next = new;
+		head->prev = new;
+
+	}
+	return (new);
+}
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,8 +9,14 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *head = *h;
+	size_t len = 0;
 
-	if (!*h && idx != 0)
+	len = dlistint_len(*h);
+	if (!*h || idx == 0)
+		return (add_dnodeint(h, n));
+	else if (idx == len)
+		return (add_dnodeint_end(h, n));
+	else if (idx > len)
 		return (NULL);
 	while (idx--)
 	{
@@ -18,25 +24,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		head = head->next;
 	}
-	if (!head->next)
-	{
-		new = add_dnodeint_end(h, n);
-	}
-	else if (!head->prev && head->next != NULL)
-	{
-		new = add_dnodeint(h, n);
-	}
-	else
-	{
-		new = malloc(sizeof(dlistint_t));
-		if (!new)
-			return (NULL);
-		new->n = n;
-		new->next = head;
-		new->prev = head->prev;
-		head->prev->next = new;
-		head->prev = new;
-		
-	}
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = head;
+	new->prev = head->prev;
+	head->prev->next = new;
+	head->prev = new;
 	return (new);
+}
+/**
+  * dlistint_len - Return number of nodes of a dlistint_t list
+  * dlistint_t - struct type
+  * @h: head
+  * Return: Number of nodes
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t x = 0;
+
+	if (h == NULL)
+		return (x);
+	while (h != NULL)
+	{
+		h = h->next;
+		x++;
+	}
+	return (x);
 }

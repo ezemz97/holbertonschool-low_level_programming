19eaeed25b794ac2da48c0e10e14d2f567f3c3dc
Eze <ezedksl@gmail.com>
2020-12-14 00:53:03 +0000

    fixed betty 40 lines

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *head;
+
+	while (idx--)
+	{
+		if (!head->next)
+			return (NULL);
+		head = head->next;
+	}
+	if (!*h || !(*h)->prev)
+		new = add_dnodeint(h, n);
+	else if (!head->next)
+		new = add_dnodeint_end(h, n);
+	else
+	{
+		new = malloc(sizeof(dlistint_t));
+			if (!new)
+		return (NULL);
+		head->prev->next = new;
+		head->next->prev = new;
+		new->next = head;
+		new->prev = head->prev;
+	}
+	return (new);
+}

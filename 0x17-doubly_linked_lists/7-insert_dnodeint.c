397eb51accaff8bcb64bbbda4a2d85887351d72c
Eze <ezedksl@gmail.com>
2020-12-14 01:19:14 +0000

    went back from messing with functions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,6 +8,49 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new, *head;
+	
+	head = *h;
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	while (idx--)
+	{
+		if (!head->next)
+		{
+			free(new);
+			return (NULL);
+		}
+		head = head->next;
+	}
+	new->n = n;
+	if (!*h)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		*h = new;
+	}
+	else if (!head->next)
+	{
+		head->next = new;
+		new->next = NULL;
+		new->prev = head;
+	}
+	else if (!head->prev)
+	{
+		head->prev = new;
+		new->next = head;
+		new->prev = NULL;
+	}
+	else
+	{
+		head->prev->next = new;
+		head->next->prev = new;
+		new->next = head;
+		new->prev = head->prev;
+	}
+	return (new);
+	/*
 	dlistint_t *new, *head = *h;
 
 	if (!*h)
@@ -34,4 +77,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->n = n;
 	}
 	return (new);
+	*/
 }

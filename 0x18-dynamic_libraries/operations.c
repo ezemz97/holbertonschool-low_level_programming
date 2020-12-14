c01d01b66b35b7b3fe3b5bf8a290f0c75219b00e
ezedksl <ezedksl@gmail.com>
2020-12-14 20:32:19 +0000

    update

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,20 @@
+add(int a, int b)
+{
+	return (a + b);
+}
+sub(int a, int b)
+{
+	return (a - b);
+}
+mul(int a, int b)
+{
+	return (a * b);
+}
+div(int a, int b)
+{
+	return (a / b);
+}
+mod(int a, int b)
+{
+	return (a % b);
+}

ba54fedb09fa6c8dbce817e5b99f368f68ff03ed
ezedksl <ezedksl@gmail.com>
2021-01-15 00:24:29 +0000

    update

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table (NULL if something goes wrong)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	
+}

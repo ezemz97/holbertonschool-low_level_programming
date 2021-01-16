5763f6af153139a8fcb3c8430187618f8b63df7e
Eze <ezedksl@gmail.com>
2021-01-16 01:54:54 +0000

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,13 +1,13 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_get - Retrieves a value associated with a key
-  * @ht: The hash table to look into
-  * @key: The key to finding
-  *
-  * Return: The value associated with the element
-  * or NULL if key couldn't be found
-  */
+* hash_table_get - Retrieves a value associated with a key
+* @ht: hash table
+* @key: key to find
+*
+* Return: The value associated with the key
+* or NULL if key couldn't be found
+*/
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;

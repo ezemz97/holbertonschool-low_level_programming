9efd8637cce075c0c3bb926ecec4a3e47fb4fe50
S <ezedksl@gmail.com>
2021-04-19 20:47:59 +0000

    add task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -7,37 +7,53 @@
  *
  * Return: first index where value is located
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-	size_t mid = size / 2;
+	int index = 0;
 
-	printf("Searching in array: ");
-	for (size_t i = 0; i < size - 1; i++)
-		printf("%d, ", array[i]);
-	printf("\n");
-	if (array == NULL)
+	if (!array)
 		return (-1);
-	if (size <= 1)
+	index = forUchecker(array, value, 0, size - 1, size);
+	return (index);
+}
+
+/**
+ * forUchecker - searches for a value in an array of integers
+ * @array: pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search
+ * @low: lowest value of the subarray
+ * @high: highest value of the subarray
+ *
+ * Return: first index where value is located
+ */
+int forUchecker(int *array, int value, int low, int high, size_t size)
+{
+	int mid = low + (high - low) / 2;
+
+	if (high >= low)
 	{
-		if (array[0] == value)
-			return (0);
-		else
-			return (-1);
-	}
-	if (array[mid] == value)
-	{
-		return (mid);
-		
-	}
-	if (array[mid] > value)
-		return (binary_search(array, mid, value));
-	if (array[mid] < value)
-	{
-		if (mid % 2 != 0)
-			return (binary_search(array, size - 1, value));
-		else
-			return (binary_search(array, size, value));
+		printf("Searching in array: ");
+		for (int i = low; i <= high; i++)
+		{
+			if (i == high)
+				printf("%d", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+		printf("\n");
+
+		/* If found at mid, then return it */
+		if (array[mid] == value)
+			return (mid);
+
+		/* Search the left half */
+		if (array[mid] > value)
+			return (forUchecker(array, value, low, mid - 1, size));
+
+		/* Search the right half */
+		return (forUchecker(array, value, mid + 1, high, size));
 	}
 	return (-1);
 }
+
--- 0x1E-search_algorithms/1-main.c
+++ 0x1E-search_algorithms/1-main.c
@@ -9,14 +9,13 @@
  */
 int main(void)
 {
-    int array[] = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    };
+    int array[];
     size_t size = sizeof(array) / sizeof(array[0]);
 
     printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
     printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
     printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
-    printf("Found %d at index: %d\n", 9, binary_search(array, size, 8));
-    return (EXIT_SUCCESS);
+    printf("Found %d at index: %d\n", 200, binary_search(array, 1, 200));
+    printf("Found %d at index: %d\n", 99, binary_search(array, 1, 99));
+    printf("Found %d at index: %d\n", 70, binary_search(array, 1, 70));
 }

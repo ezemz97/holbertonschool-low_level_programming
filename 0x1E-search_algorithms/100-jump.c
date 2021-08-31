69b1d49cf7edc8624d44959dc07159abc25aa2e0
Eze <ezedksl@gmail.com>
2021-08-30 23:01:57 -0300

    update

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,18 +16,22 @@ int jump_search(int *array, size_t size, int value)
 
 	while (array[fmin(b, n) - 1 < value])
 	{
+		printf("Value checked array[%i] = [%i]\n", a, array[a]);
 		a = b;
 		b = b + sqrt(n);
 		if (a >= n)
 			return (-1);
 	}
+	printf("BB: %d\n", b);
 	while (array[a] < value)
 	{
+		printf("Value checked array[%i] = [%i]\n", a, array[a]);
 		a++;
 		if (a == fmin(b, n))
 			return (-1);
 	}
 	if (array[a] == value)
+
 		return (a);
 
 	return (-1);
